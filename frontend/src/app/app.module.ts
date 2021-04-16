d859c1cbc482443bbd2a0b4a5e288c1b751c68e1
anjalig <anjalig1109@gmail.com>
2021-04-16 16:48:49 +0530

    my changes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { AuthorsComponent } from './authors/authors.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BooksComponent
+    BooksComponent,
+    AuthorsComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
